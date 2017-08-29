92028d1c543c0eb3a6c8778a1aa577557e93b824
liang <ldong6@ncsu.edu>
2017-08-28 23:08:45 -0400

    update evaluation_coco.py

--- evaluation_coco.py
+++ evaluation_coco.py
@@ -2,44 +2,33 @@
 ## you don't need to include it.
 import sys
 sys.path.append('../../practice_demo')
-
 import os
-import mxnet as mx
-import numpy as np
 import copy
 import re
-from google.protobuf import text_format
 import json
+from collections import namedtuple
+from scipy.ndimage.filters import gaussian_filter
+
+import mxnet as mx
+
+import numpy as np
+from numpy import ma
+
+from google.protobuf import text_format
+
 import cv2 as cv
 import scipy
 import PIL.Image
 import math
 import time
-import scipy
-import matplotlib
-import pylab as plt
-import matplotlib.pyplot as plt
+import skimage.io as io
+
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-import numpy as np
-import skimage.io as io
-import pylab
-from scipy.ndimage.filters import gaussian_filter
+
 from GenerateLabelCPM import *
 from modelCPM import *
-from collections import namedtuple
-import math
-import numpy as np
-from numpy import ma
-import matplotlib.pyplot as plt
-import matplotlib
-import matplotlib.pyplot as plt
-from pycocotools.coco import COCO
-from pycocotools.cocoeval import COCOeval
-import numpy as np
-import skimage.io as io
-import pylab
-pylab.rcParams['figure.figsize'] = (10.0, 8.0) 
+
 
 Point = namedtuple('Point', 'x y')
 crop_size_x = 368
@@ -119,6 +108,9 @@ def applyDNN(oriImg, images, sym1, arg_params1, aux_params1):
     testimage = transposeImage
     # print testimage.shape
     cmodel = mx.mod.Module(symbol=sym1, label_names=[])
+    # cmodel = mx.mod.Module(symbol=sym1, label_names=[], context=mx.gpu(0))
+    # cmodel = mx.mod.Module(symbol=sym1, context = mx.gpu(3), label_names=[])
+    # cmodel = mx.mod.Module(symbol = sym1, label_names = []) 
     cmodel.bind(data_shapes=[('data', (1, 3, testimage.shape[1], testimage.shape[2]))])
     cmodel.init_params(arg_params=arg_params1, aux_params=aux_params1)
     # print 'init_params failed'
@@ -129,7 +121,7 @@ def applyDNN(oriImg, images, sym1, arg_params1, aux_params1):
     result=cmodel.get_outputs()
     
     heatmap = np.moveaxis(result[1].asnumpy()[0], 0, -1)
-    heatmap = cv.resize(heatmap, (0,0), fx=8, fy=8, interpolation=cv.INTER_CUBIC)
+    heatmap = cv.resize(heatmap, (0,0), fx=8, fy=8, interpolation=cv.INTER_CUBIC) # INTER_LINEAR
     heatmap = heatmap[:imageToTest_padded.shape[0]-pad[2], :imageToTest_padded.shape[1]-pad[3], :]
     heatmap = cv.resize(heatmap, (oriImg.shape[1], oriImg.shape[0]), interpolation=cv.INTER_CUBIC)
     
@@ -151,9 +143,16 @@ def applyModel(oriImg, param, sym, arg_params, aux_params):
     
     makeFigure = 0 
     numberPoints = 1
+    
+    '''
     octave = param['octave']
     starting_range = param['starting_range']
     ending_range = param['ending_range']
+    '''
+    '''
+    octave = 6
+    starting_range = 0.85 # 0.25 0.7
+    ending_range = 1.5    # 1.2 1.8
     starting_scale = boxsize*1.0/(oriImg.shape[0]*ending_range)
     ending_scale = boxsize*1.0/(oriImg.shape[0]*starting_range)
     
@@ -165,7 +164,10 @@ def applyModel(oriImg, param, sym, arg_params, aux_params):
         # print current_scale
         multiplier.append(pow(2, current_scale))
         current_scale = current_scale+(1.0/octave)
-        
+    '''
+    boxsize = 368
+    scale_search = 0.5, 1.0, 1.5, 2.0
+    multiplier = [x * boxsize / oriImg.shape[0] for x in scale_search]
     # print multiplier
     
     heatmap_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 19))
@@ -223,8 +225,9 @@ def connect56LineVec(oriImg, param, sym, arg_params, aux_params):
     connection_all = []
     special_k = []
     special_non_zero_index = []
-    mid_num = 11
+    mid_num = 10
 
+    
     for k in range(len(mapIdx)):
         score_mid = paf_avg[:,:,[x-19 for x in mapIdx[k]]]
         candA = all_peaks[limbSeq[k][0]-1]
@@ -240,35 +243,38 @@ def connect56LineVec(oriImg, param, sym, arg_params, aux_params):
             connection_candidate = []
             for i in range(nA):
                 for j in range(nB):
-                    vec = np.subtract(candB[j][:2], candA[i][:2])
-                    # print('vec: ',vec)
-                    norm = math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
-                    # print('norm: ', norm)
-                    vec = np.divide(vec, norm)
-                    # print('normalized vec: ', vec)
-                    startend = zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
-                                   np.linspace(candA[i][1], candB[j][1], num=mid_num))
-                    # print('startend: ', startend)
-                    vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
-                                      for I in range(len(startend))])
-                    # print('vec_x: ', vec_x)
-                    vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
-                                      for I in range(len(startend))])
-                    # print('vec_y: ', vec_y)
-                    score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
-                    # print(score_midpts)
-                    # print('score_midpts: ', score_midpts)
-                    score_with_dist_prior = sum(score_midpts)/len(score_midpts) + min(0.5*oriImg.shape[0]/norm-1, 0)
+                    try:
+                        vec = np.subtract(candB[j][:2], candA[i][:2])
+                        # print('vec: ',vec)
+                        norm = math.sqrt(vec[0]*vec[0] + vec[1]*vec[1])
+                        # print('norm: ', norm)
+                        vec = np.divide(vec, norm)
+                        # print('normalized vec: ', vec)
+                        startend = zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
+                                       np.linspace(candA[i][1], candB[j][1], num=mid_num))
+                        # print('startend: ', startend)
+                        vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
+                                          for I in range(len(startend))])
+                        # print('vec_x: ', vec_x)
+                        vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
+                                          for I in range(len(startend))])
+                        # print('vec_y: ', vec_y)
+                        score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
+                        # print(score_midpts)
+                        # print('score_midpts: ', score_midpts)
+                        score_with_dist_prior = sum(score_midpts)/len(score_midpts) + min(0.5*oriImg.shape[0]/norm-1, 0)
 
-                    # print('score_with_dist_prior: ', score_with_dist_prior)
-                    criterion1 = len(np.nonzero(score_midpts > param['thre2'])[0]) > 0.8 * len(score_midpts)
-                    # print('score_midpts > param["thre2"]: ', len(np.nonzero(score_midpts > param['thre2'])[0]))
-                    criterion2 = score_with_dist_prior > 0
+                        # print('score_with_dist_prior: ', score_with_dist_prior)
+                        criterion1 = len(np.nonzero(score_midpts > param['thre2'])[0]) > 0.8 * len(score_midpts)
+                        # print('score_midpts > param["thre2"]: ', len(np.nonzero(score_midpts > param['thre2'])[0]))
+                        criterion2 = score_with_dist_prior > 0
 
-                    if criterion1 and criterion2:
-                        # print('match')
-                        # print(i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2])
-                        connection_candidate.append([i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2]])
+                        if criterion1 and criterion2:
+                            # print('match')
+                            # print(i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2])
+                            connection_candidate.append([i, j, score_with_dist_prior, score_with_dist_prior+candA[i][2]+candB[j][2]])
+                    except:
+                        print 'error rendering'
                     # print('--------end-----------')
             connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
             # print('-------------connection_candidate---------------')
@@ -286,7 +292,8 @@ def connect56LineVec(oriImg, param, sym, arg_params, aux_params):
                         break
 
             connection_all.append(connection)
-        elif(nA != 0 or nB != 0):
+        # elif(nA != 0 or nB != 0):
+        else:
             special_k.append(k)
             special_non_zero_index.append(indexA if nA != 0 else indexB)
             connection_all.append([])
@@ -299,51 +306,54 @@ def connect56LineVec(oriImg, param, sym, arg_params, aux_params):
 
     for k in range(len(mapIdx)):
         if k not in special_k:
-            partAs = connection_all[k][:,0]
-            partBs = connection_all[k][:,1]
-            indexA, indexB = np.array(limbSeq[k]) - 1
+            try:
+                partAs = connection_all[k][:,0]
+                partBs = connection_all[k][:,1]
+                indexA, indexB = np.array(limbSeq[k]) - 1
 
-            for i in range(len(connection_all[k])): #= 1:size(temp,1)
-                found = 0
-                subset_idx = [-1, -1]
-                for j in range(len(subset)): #1:size(subset,1):
-                    if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
-                        subset_idx[found] = j
-                        found += 1
+                for i in range(len(connection_all[k])): #= 1:size(temp,1)
+                    found = 0
+                    subset_idx = [-1, -1]
+                    for j in range(len(subset)): #1:size(subset,1):
+                        if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
+                            subset_idx[found] = j
+                            found += 1
 
-                if found == 1:
-                    j = subset_idx[0]
-                    if(subset[j][indexB] != partBs[i]):
-                        subset[j][indexB] = partBs[i]
-                        subset[j][-1] += 1
-                        subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
-                elif found == 2: # if found 2 and disjoint, merge them
-                    j1, j2 = subset_idx
-                    print "found = 2"
-                    membership = ((subset[j1]>=0).astype(int) + (subset[j2]>=0).astype(int))[:-2]
-                    if len(np.nonzero(membership == 2)[0]) == 0: #merge
-                        subset[j1][:-2] += (subset[j2][:-2] + 1)
-                        subset[j1][-2:] += subset[j2][-2:]
-                        subset[j1][-2] += connection_all[k][i][2]
-                        subset = np.delete(subset, j2, 0)
-                    else: # as like found == 1
-                        subset[j1][indexB] = partBs[i]
-                        subset[j1][-1] += 1
-                        subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+                    if found == 1:
+                        j = subset_idx[0]
+                        if(subset[j][indexB] != partBs[i]):
+                            subset[j][indexB] = partBs[i]
+                            subset[j][-1] += 1
+                            subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+                    elif found == 2: # if found 2 and disjoint, merge them
+                        j1, j2 = subset_idx
+                        print "found = 2"
+                        membership = ((subset[j1]>=0).astype(int) + (subset[j2]>=0).astype(int))[:-2]
+                        if len(np.nonzero(membership == 2)[0]) == 0: #merge
+                            subset[j1][:-2] += (subset[j2][:-2] + 1)
+                            subset[j1][-2:] += subset[j2][-2:]
+                            subset[j1][-2] += connection_all[k][i][2]
+                            subset = np.delete(subset, j2, 0)
+                        else: # as like found == 1
+                            subset[j1][indexB] = partBs[i]
+                            subset[j1][-1] += 1
+                            subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
 
-                # if find no partA in the subset, create a new subset
-                elif not found and k < 17:
-                    row = -1 * np.ones(20)
-                    row[indexA] = partAs[i]
-                    row[indexB] = partBs[i]
-                    row[-1] = 2
-                    row[-2] = sum(candidate[connection_all[k][i,:2].astype(int), 2]) + connection_all[k][i][2]
-                    subset = np.vstack([subset, row])
+                    # if find no partA in the subset, create a new subset
+                    elif not found and k < 17:
+                        row = -1 * np.ones(20)
+                        row[indexA] = partAs[i]
+                        row[indexB] = partBs[i]
+                        row[-1] = 2
+                        row[-2] = sum(candidate[connection_all[k][i,:2].astype(int), 2]) + connection_all[k][i][2]
+                        subset = np.vstack([subset, row])
+            except:
+                print "not link"
 
     # delete some rows of subset which has few parts occur
     deleteIdx = [];
     for i in range(len(subset)):
-        if subset[i][-1] < 4 or subset[i][-2]/subset[i][-1] < 0.4:
+        if subset[i][-1] < 3 or subset[i][-2]/subset[i][-1] < 0.2:
             deleteIdx.append(i)
     subset = np.delete(subset, deleteIdx, axis=0)
     
@@ -402,19 +412,33 @@ if modelId == 2:
                                      'Leye', 'Reye', 'Lear', 'Rear', 'pt19']
 
 # candidate
-orderCOCO = [1,0,7,9,11, 6,8,10, 13,15,17, 12,14,16, 3,2,5,4]
+starttime = time.time()
+orderCOCO = [1,0,7,9,11, 6,8,10,13,15,17,12,14,16,3,2,5,4]
 myjsonValidate = list(dict())
 count = 0
-imgIds_num = 3
+imgIds_num = 2644
+#imgIds_num = 100
+not_working_num = 0
+notworkingimageIds = []
 # imgIds_num = len(imgIds)
 for i in range(imgIds_num):
+    print 'image: ', i
     img = cocoGt.loadImgs(imgIds[i])[0]
     cimg = io.imread('/data/guest_users/liangdong/liangdong/practice_demo/val2014/'+img['file_name'])
+    
+    if len(cimg.shape)==2:       
+        cimgRGB = np.zeros((cimg.shape[0], cimg.shape[1], 3))
+        for i in range(3):
+            cimgRGB[:, :, i] = cimg
+            print cimgRGB.shape
+    else:
+        cimgRGB = cimg
+        
     # print 'image shape'
     # print cimg.shape
     image_id = img['id']
     try:
-        candidate, subset = connect56LineVec(cimg, param, sym, arg_params, aux_params)
+        candidate, subset = connect56LineVec(cimgRGB, param, sym, arg_params, aux_params)
         print subset
         
         for j in range(len(subset)):
@@ -447,8 +471,10 @@ for i in range(imgIds_num):
             count = count + 1
             
     except:
-        print 'train image'
+        print 'train image not working'
         print image_id
+        notworkingimageIds.append(image_id)
+        not_working_num = not_working_num+1
 
 annType = ['segm','bbox','keypoints']
 annType = annType[2]      #specify type here
@@ -465,11 +491,16 @@ image_ids = []
 for i in range(imgIds_num):
     img = cocoGt.loadImgs(imgIds[i])[0]
     image_ids.append(img['id'])
-    
+
+print 'len: ', len(image_ids)
+print 'not working number ', not_working_num
+print  notworkingimageIds
 # running evaluation
 cocoEval = COCOeval(cocoGt, cocoDt2, annType)
 cocoEval.params.imgIds  = image_ids
 cocoEval.evaluate()
 cocoEval.accumulate()
 k = cocoEval.summarize()
+endtime = time.time()
 print k
+print (endtime-starttime)/60.0
