9df4209bd8d763cd7fd5a57e1c2406f2ea1aba03
liang <ldong6@ncsu.edu>
2017-12-05 22:49:44 -0500

    add trainval

--- config/config.py
+++ config/config.py
@@ -22,7 +22,7 @@ config.TEST.imgIds_num = 5
 
 config.TRAIN = edict()
 
-config.TRAIN.num_epoch = 1
+config.TRAIN.num_epoch = 3
 
 config.TRAIN.initial_model = '../realtimePose'
 
@@ -36,17 +36,19 @@ config.TRAIN.center_perterb_max = 0
 
 config.TRAIN.scale_prob = 1
 
-config.TRAIN.scale_min = 0.5
-
 config.TRAIN.flip = False
 
 config.TRAIN.scale_max = 1.1
+
+config.TRAIN.scale_min = 0.5
 
 config.TRAIN.target_dist = 0.6
 
 config.TRAIN.max_rotate_degree = 0
 
-config.TRAIN.scale_set = False
+config.TRAIN.scale_set = True
+
+config.TRAIN.augmentation = True
 
 config.TRAIN.head = 'vgg'
 
